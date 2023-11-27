0c7664eea8c9d1b69766fdf706456742497b8c8c
Walter Shands <walter.shands@biogen.com>
2023-11-27 12:17:23 -0800

    fix lambda file name in Dockerfile and use image

--- main.tf
+++ main.tf
@@ -55,14 +55,14 @@ resource "aws_s3_object" "lambda_dx_symlink_internal" {
 
 resource "aws_lambda_function" "dx_symlink_internal_lambda" {
   function_name = "DxSymlinkInternal"
+  #s3_bucket = aws_s3_bucket.lambda_bucket.id
+  #s3_key    = aws_s3_object.lambda_dx_symlink_internal.key
+  #runtime = "python3.9"
+  #handler = "dx_symlink_internal_lambda.lambda_handler"
+  #source_code_hash = data.archive_file.lambda_dx_symlink_internal.output_base64sha256
 
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_dx_symlink_internal.key
-
-  runtime = "python3.9"
-  handler = "dx_symlink_internal_lambda.lambda_handler"
-
-  source_code_hash = data.archive_file.lambda_dx_symlink_internal.output_base64sha256
+  image_uri     = "230407893272.dkr.ecr.us-east-1.amazonaws.com/dnanexus_symlink_internal:main"
+  package_type  = "Image"
 
   role = aws_iam_role.lambda_exec.arn
 }
@@ -95,3 +95,36 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+
+
+# Add Lambda trigger from S3 bucket
+# A file added to the proper bucket will trigger the Lambda
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification
+# https://stackoverflow.com/questions/68245765/add-trigger-to-aws-lambda-functions-via-terraform
+
+# use 'data' to referece an already existing bucket
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/s3_bucket
+data "aws_s3_bucket" "transfer-bucket" {
+  bucket = "transferred-files"
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dx_symlink_internal_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = data.aws_s3_bucket.transfer-bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = data.aws_s3_bucket.transfer-bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.dx_symlink_internal_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    #filter_prefix       = "transferred-fles/"
+    #filter_suffix       = ".log"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
