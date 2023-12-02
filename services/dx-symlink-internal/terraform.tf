1d8a3ff067e2acc7c33419cd330830f9e55cda86
Walter Shands <walter.shands@biogen.com>
2023-12-01 20:56:01 -0800

    trigger on any file change in dir

--- services/dx-symlink-internal/terraform.tf
+++ services/dx-symlink-internal/terraform.tf
@@ -1,12 +1,5 @@
 
 terraform {
-
-  # cloud {
-  #   workspaces {
-  #     name = "learn-terraform-lambda-api-gateway"
-  #   }
-  # }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
