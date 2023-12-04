4c0f924de419c935d879f939bd996900b2670393
Walter Shands <walter.shands@biogen.com>
2023-12-04 10:42:29 -0800

    add deleter files

---
+++ services/dx-symlink-deleter/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      Biogen = "symlink-s3-files-in-DNAnexus"
+    }
+  }
+}
+
+module "symlink_lambda" {   
+  source = "../modules/dx-symlink-lambda"
+  lambda_function_name = "DxSymlinkDeleter"
+  lambda_image_uri = "230407893272.dkr.ecr.us-east-1.amazonaws.com/dnanexus_symlink_deleter:main"
+}
---
+++ services/dx-symlink-deleter/outputs.tf
@@ -0,0 +1,9 @@
+
+# Output value definitions
+
+output "function_name" {
+  description = "Name of the Lambda function."
+
+  value = module.symlink_lambda.function_name
+}
+
