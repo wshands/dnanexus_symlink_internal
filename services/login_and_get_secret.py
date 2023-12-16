c459294dde0248a2644b4864d3741b5606a2225c
Walter Shands <walter.shands@biogen.com>
2023-12-15 17:38:06 -0800

    use logger and double quotes

--- services/login_and_get_secret.py
+++ services/login_and_get_secret.py
@@ -1,19 +1,23 @@
 import boto3
 from botocore.exceptions import ClientError
 import dxpy
+import logging
 import json
 import os
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def get_secret():
-    secret_name = os.environ['DNANEXUS_TOKEN_SECRET_NAME']
-    secret_key = os.environ['DNANEXUS_TOKEN_SECRET_KEY']
+    secret_name = os.environ["DNANEXUS_TOKEN_SECRET_NAME"]
+    secret_key = os.environ["DNANEXUS_TOKEN_SECRET_KEY"]
     # https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime   
-    region_name = os.environ['AWS_REGION']
+    region_name = os.environ["AWS_REGION"]
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
-        service_name='secretsmanager',
+        service_name="secretsmanager",
         region_name=region_name
     )
 
@@ -27,17 +31,19 @@ def get_secret():
         raise e
 
     # Decrypts secret using the associated KMS key.
-    secret = json.loads(get_secret_value_response['SecretString'])[secret_key]
+    secret = json.loads(get_secret_value_response["SecretString"])[secret_key]
     return secret
 
 def login(token):
     try:
-        dxpy.set_api_server_info(host='api.dnanexus.com', protocol='https')
+        dxpy.set_api_server_info(host="api.dnanexus.com", protocol="https")
         dxpy.set_security_context({"auth_token_type": "Bearer", "auth_token": token})
         dxpy.set_workspace_id(None)
-        print('Logged as', dxpy.whoami())
+        #print("Logged as", dxpy.whoami())
+        logger.info(f"Logged in as:{dxpy.whoami()}")
     except dxpy.exceptions.InvalidAuthentication as e:
-        print('Login failed!')
-        print(e)
+        #print("Login failed!")
+        #print(e)
+        logger.info(f"Login failed!:{e}")
         exit(1)
         
